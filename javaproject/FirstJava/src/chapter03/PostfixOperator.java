1ab107ab051f582338e6a72d8aa5408c0480baa0
himediahj <himedia.hj@gmail.com>
2022-09-14 17:04:33 +0900

    Update PostfixOperator.java

--- javaproject/FirstJava/src/chapter03/PostfixOperator.java
+++ javaproject/FirstJava/src/chapter03/PostfixOperator.java
@@ -13,6 +13,8 @@ public class PostfixOperator {
 		System.out.println("num1 : " + num1);
 		System.out.println("num2 : " + num2);
 		
+		System.out.println("num1 : " + num1++); // num1 = 6 그 후 7이 됨
+		
 	}
 
 }
