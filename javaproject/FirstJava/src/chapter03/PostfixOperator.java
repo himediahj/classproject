b35c71a7edc176d88e33407b850a8ec9a1ffef11
himediahj <himedia.hj@gmail.com>
2022-09-14 16:19:49 +0900

    연산자 예제코드
    단항연산자 예제

---
+++ javaproject/FirstJava/src/chapter03/PostfixOperator.java
@@ -0,0 +1,18 @@
+package chapter03;
+
+public class PostfixOperator {
+
+	public static void main(String[] args) {
+
+		int num1 = 5;
+		
+		System.out.println("num1 : " + num1);
+		
+		int num2 = num1++;   // num2 = num1 그 후 num1 = num1 + 1
+							 // num2 = 5, num1 = 6
+		System.out.println("num1 : " + num1);
+		System.out.println("num2 : " + num2);
+		
+	}
+
+}
