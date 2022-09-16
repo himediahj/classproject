5da0a458785e514c7bf4be51edbaebda22e1d98e
himediahj <himedia.hj@gmail.com>
2022-09-16 16:42:11 +0900

    continue

---
+++ javaproject/FirstJava/src/chapter04/ContinueTest.java
@@ -0,0 +1,21 @@
+package chapter04;
+
+public class ContinueTest {
+
+	public static void main(String[] args) {
+		
+		// 1~9 사이의 홀수만 출력
+		
+		int num = 0;
+		
+		while(++num < 10) {
+			if(num %2 == 0) {
+				continue;
+			}
+			System.out.println(num);
+			
+			// num++;	continue 처리되면 ++ 처리 안되니까 while 조건식에 써 줘
+		}
+	}
+
+}
