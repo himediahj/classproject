a65ed5e14c495913bdb9f8c4df15052e52aafce6
himediahj <himedia.hj@gmail.com>
2022-09-23 18:30:02 +0900

    mini project

---
+++ javaproject/OOPMiniProject/src/ver02/SmartPhoneMain.java
@@ -0,0 +1,13 @@
+package ver02;
+
+public class SmartPhoneMain {
+
+	public static void main(String[] args) {
+
+		SmartPhone smartPhone = SmartPhone.getInstance();
+		
+		smartPhone.insert();
+		smartPhone.modify();
+	}
+
+}
