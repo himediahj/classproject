1a64072b3f4b34671067af980fe15a747b93fda0
himediahj <himedia.hj@gmail.com>
2022-09-28 13:43:52 +0900

    API

---
+++ javaproject/FirstJava/src/chapter09/MathTest.java
@@ -0,0 +1,27 @@
+package chapter09;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		// PI
+		System.out.println(Math.PI);
+		
+		// 난수 생성
+		System.out.println(Math.random());
+		// 1~45
+		int num = (int)(Math.random()*45)+1;
+		System.out.println(num);
+		
+		// 절사
+		System.out.println(Math.floor(3.0002));
+		
+		// 반올림
+		System.out.println(Math.round(3.3002));
+		
+		System.out.println(Math.max(10, 20));
+		System.out.println(Math.min(10, 20));
+		
+
+	}
+
+}
