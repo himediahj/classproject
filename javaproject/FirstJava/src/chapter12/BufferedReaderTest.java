982f1b5062500ef61d1d8ae01b6d9218e7dba383
himediahj <himedia.hj@gmail.com>
2022-10-06 17:32:05 +0900

    IO

---
+++ javaproject/FirstJava/src/chapter12/BufferedReaderTest.java
@@ -0,0 +1,35 @@
+package chapter12;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderTest {
+
+	public static void main(String[] args) {
+
+		try (BufferedReader in = new BufferedReader(new FileReader("C:\\test\\text2.txt"))) {
+			while (true) {
+				String str = in.readLine();	// 행 단위로 읽어오기
+				
+				if(str == null)
+					break;
+				
+				System.out.println(str);
+
+			}
+			
+			System.out.println("프로그램 종료");
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ javaproject/FirstJava/src/chapter12/FileInputStreamTest2.java
@@ -0,0 +1,46 @@
+package chapter12;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamTest2 {
+	public static void main(String[] args) {
+		// 파일 읽기 : InputStream -> FileInputStream
+		FileInputStream in = null;
+		
+		try {
+			in = new FileInputStream("C:\\test\\testfile2.txt");
+			
+			while(true) {
+				int data = in.read();
+				if(data == -1) {	// 반환할 문자 없으면 -1 반환
+					break;
+				}
+				System.out.print((char)data);
+			}
+			
+			System.out.println("\n데이터 읽기 성공!");
+			
+
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			
+			if(in != null) {
+
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+}
