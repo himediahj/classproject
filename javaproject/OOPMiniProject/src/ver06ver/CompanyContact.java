0650ee1d72a033690c0122d7b3a74ecde4c5240b
himediahj <himedia.hj@gmail.com>
2022-10-06 17:25:37 +0900

    과제용

---
+++ javaproject/OOPMiniProject/src/ver06ver/CompanyContact.java
@@ -0,0 +1,52 @@
+package ver06ver;
+
+public class CompanyContact extends Contact {
+
+	private String companyName;
+	private String departmentName;
+	private String position;
+
+	public CompanyContact(String name, String phoneNumber, String email, String address, String birth, String group,
+			String companyName, String departmentName, String position) {
+		super(name, phoneNumber, email, address, birth, group);
+		this.companyName = companyName;
+		this.departmentName = departmentName;
+		this.position = position;
+	}
+
+	@Override
+	void printAll() {
+		super.printAll();
+		System.out.println("회사 이름 : " + this.companyName);
+		System.out.println("부서 이름 : " + this.departmentName);
+		System.out.println("직급 : " + this.position);
+
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	
+
+}
