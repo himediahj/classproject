d68bd75c132414a703e5d8c4dd176272040fb5ee
himediahj <himedia.hj@gmail.com>
2022-11-22 14:21:57 +0900

    Todo Spring

---
+++ spring/intellij/TodoSpring/src/main/java/com/todo/todospring/controller/MemberRegController.java
@@ -0,0 +1,35 @@
+package com.todo.todospring.controller;
+
+import com.todo.todospring.domain.MemberRegRequest;
+import com.todo.todospring.service.MemberRegService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+
+@Controller
+@RequestMapping("/member/register")
+@Log4j2
+public class MemberRegController {
+    @Autowired
+    private MemberRegService regService;
+
+    @GetMapping
+    public String getRegForm(){
+        return "member/regForm";
+    }
+
+    @PostMapping
+    public String reg(MemberRegRequest regRequest, HttpServletRequest request) throws SQLException {
+        log.info(regRequest);
+        log.info(regRequest.toMember());
+
+        regService.memberReg(regRequest, request);
+        return "redirect:/index.jsp";
+    }
+}
---
+++ spring/intellij/TodoSpring/src/main/java/com/todo/todospring/service/LoginService.java
@@ -0,0 +1,22 @@
+package com.todo.todospring.service;
+
+import com.todo.todospring.dao.MemberDao;
+import com.todo.todospring.domain.Member;
+import com.todo.todospring.util.ConnectionProvider;
+import lombok.Cleanup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Service
+public class LoginService {
+    @Autowired
+    private MemberDao memberDao;
+
+    public Member login(String uid, String pw) throws SQLException {
+        @Cleanup Connection conn = ConnectionProvider.getInstance().getConnection();
+        return memberDao.selectByIdPw(conn, uid, pw);
+    }
+}
