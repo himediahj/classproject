e5ce9d66b081439fd86a350ec582016414151582
himediahj <himedia.hj@gmail.com>
2022-11-26 12:31:09 +0900

    회원가입 validation

--- spring/intellij/TodoList/src/main/java/com/spring/todolist/domain/MemberRegRequest.java
+++ spring/intellij/TodoList/src/main/java/com/spring/todolist/domain/MemberRegRequest.java
@@ -17,7 +17,7 @@ public class MemberRegRequest {
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String pw;
     @NotBlank
-    @Pattern(regexp = "[a-zA-Z가-힣]{1,64}", message = "빈값이나 공백이 들어갈 수 없습니다.")
+    @Pattern(regexp = "[a-zA-Z가-힣]{1,64}", message = "영문과 한글만 입력 가능합니다.")
     private String uname;
     private MultipartFile uphoto;
 
